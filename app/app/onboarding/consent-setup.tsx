c403ae0acd093600517b9d6c9ba604d7a74b3b74
Branden Algozino <softnessasastrength@gmail.com>
2026-07-11 18:36:35 +0000

    feat: scaffold consent setup screen

---
+++ app/app/onboarding/consent-setup.tsx
@@ -0,0 +1,6 @@
+import { Text } from 'react-native';
+import { PocScreen } from '../../components/PocScreen';
+
+export default function ConsentSetupScreen() {
+  return <PocScreen title="Consent Setup"><Text>Body-zone, pressure, and hard-stop preferences will be configured here.</Text></PocScreen>;
+}
