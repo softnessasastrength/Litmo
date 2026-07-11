ee4a663b7661de93a91d1b4d1f764990e231627f
Branden Algozino <softnessasastrength@gmail.com>
2026-07-11 18:36:27 +0000

    feat: scaffold touch language screen

---
+++ app/app/onboarding/touch-language.tsx
@@ -0,0 +1,6 @@
+import { Text } from 'react-native';
+import { PocScreen } from '../../components/PocScreen';
+
+export default function TouchLanguageScreen() {
+  return <PocScreen title="Touch Language Profile"><Text>Hold types, pressure, duration, environment, and nervous-system notes will be captured here.</Text></PocScreen>;
+}
